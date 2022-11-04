3814c96d5493ce5964df3739d7ccae1fbd70be94
Johan Leduc <johan.leduc90@gmail.com>
2022-11-04 18:22:59 +0100

    Simplify random forest

--- decision_tree/jax/random_forest/classifier.py
+++ decision_tree/jax/random_forest/classifier.py
@@ -1,24 +1,10 @@
 from functools import partial
-from multiprocessing import Pool
 
 import jax
 import jax.numpy as jnp
 from jax import vmap
 
 from ..classifier import DecisionTreeClassifier
-
-
-def fit_decision_tree_classifier(data) -> DecisionTreeClassifier:
-    X, y, max_depth, min_samples, max_splits, key = data
-    model = DecisionTreeClassifier(
-        max_depth=max_depth,
-        min_samples=min_samples,
-        max_splits=max_splits,
-    )
-    n_samples = X.shape[0]
-    idx = jax.random.randint(key, shape=(n_samples,), minval=0, maxval=n_samples)
-    model.fit(X[idx], y[idx])
-    return model
 
 
 class RandomForestClassifier:
@@ -33,25 +19,33 @@ class RandomForestClassifier:
         self.min_samples = min_samples
         self.max_depth = max_depth
         self.max_splits = max_splits
-        self.estimators = []
+        self.estimators = None
 
     def fit(self, X: jnp.ndarray, y: jnp.ndarray) -> None:
         X = X.astype("float32")
         y = y.astype("int16")
         self.n_classes = jnp.size(jnp.bincount(y))
 
-        def params():
-            key = jax.random.PRNGKey(seed=0)
-            for _ in range(self.n_estimators):
-                key, subkey = jax.random.split(key)
-                yield [X, y, self.max_depth, self.min_samples, self.max_splits, subkey]
-
-        with Pool() as p:
-            self.estimators = p.map(fit_decision_tree_classifier, params())
-
-        return
+        self.estimators = []
+        key = jax.random.PRNGKey(seed=0)
+        n_samples = X.shape[0]
+        for _ in range(self.n_estimators):
+            key, subkey = jax.random.split(key)
+            model = DecisionTreeClassifier(
+                max_depth=self.max_depth,
+                min_samples=self.min_samples,
+                max_splits=self.max_splits,
+            )
+            idx = jax.random.randint(
+                subkey, shape=(n_samples,), minval=0, maxval=n_samples
+            )
+            model.fit(X[idx], y[idx])
+            self.estimators.append(model)
 
     def predict(self, X: jnp.ndarray) -> jnp.ndarray:
+        if self.estimators is None:
+            raise ValueError("The model is not fitted.")
+
         X = X.astype("float32")
         preds = jnp.stack(
             [estimator.predict(X) for estimator in self.estimators], axis=0

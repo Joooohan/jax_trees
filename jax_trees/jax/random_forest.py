8e20d536e474a12b678db654ae614819d1d2a3f1
Johan Leduc <johan.leduc90@gmail.com>
2022-12-01 20:26:29 +0100

    JIT predict and fit of random forest

--- jax_trees/jax/random_forest.py
+++ jax_trees/jax/random_forest.py
@@ -1,12 +1,16 @@
+from __future__ import annotations
+
 from functools import partial
 
 import jax
 import jax.numpy as jnp
-from jax import vmap
+from jax import jit, vmap
+from jax.tree_util import register_pytree_node_class
 
-from .classifier import DecisionTreeClassifier, most_frequent
+from .classifier import DecisionTreeClassifier
 
 
+@register_pytree_node_class
 class RandomForestClassifier:
     def __init__(
         self,
@@ -15,6 +19,7 @@ class RandomForestClassifier:
         min_samples: int = 2,
         max_depth: int = 4,
         max_splits: int = 25,
+        predictors=None,
     ):
         self.base_model = DecisionTreeClassifier(
             n_classes=n_classes,
@@ -23,9 +28,10 @@ class RandomForestClassifier:
             max_splits=max_splits,
         )
         self.n_estimators = n_estimators
-        self.predictors = None
+        self.predictors = predictors
 
-    def fit(self, X: jnp.ndarray, y: jnp.ndarray) -> None:
+    @jit
+    def fit(self, X: jnp.ndarray, y: jnp.ndarray) -> RandomForestClassifier:
         n_samples = X.shape[0]
         key = jax.random.PRNGKey(seed=0)
         idx = jax.random.randint(
@@ -42,7 +48,11 @@ class RandomForestClassifier:
         )(self.base_model, X, y, mask)
         return self
 
+    @jit
     def predict(self, X: jnp.ndarray) -> jnp.ndarray:
+        if self.predictors is None:
+            raise ValueError("The model is not fitted.")
+
         preds = vmap(DecisionTreeClassifier.predict, in_axes=[0, None])(
             self.predictors, X
         )
@@ -54,3 +64,19 @@ class RandomForestClassifier:
     def score(self, X: jnp.ndarray, y: jnp.ndarray) -> float:
         preds = self.predict(X)
         return self.base_model.score_fn(preds, y)
+
+    def tree_flatten(self):
+        children = [self.predictors]
+        aux_data = {
+            "min_samples": self.base_model.min_samples,
+            "max_depth": self.base_model.max_depth,
+            "max_splits": self.base_model.max_splits,
+            "n_classes": self.base_model.n_classes,
+            "n_estimators": self.n_estimators,
+        }
+        return (children, aux_data)
+
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        (predictors,) = children
+        return cls(**aux_data, predictors=predictors)

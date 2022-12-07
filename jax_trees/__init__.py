c1d11189182184c4e8a8bc248b2d0d94b10c79fe
Johan Leduc <johan.leduc90@gmail.com>
2022-12-07 17:38:33 +0100

    import in jax

--- jax_trees/__init__.py
+++ jax_trees/__init__.py
@@ -1,1 +1,19 @@
+from .jax import (
+    DecisionTreeClassifier,
+    DecisionTreeRegressor,
+    GradientBoostedClassifier,
+    GradientBoostedRegressor,
+    RandomForestClassifier,
+    RandomForestRegressor,
+)
+
 VERSION = "0.0.2"
+
+__all__ = [
+    "DecisionTreeClassifier",
+    "DecisionTreeRegressor",
+    "RandomForestClassifier",
+    "RandomForestRegressor",
+    "GradientBoostedRegressor",
+    "GradientBoostedClassifier",
+]
